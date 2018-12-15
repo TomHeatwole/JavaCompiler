9547d1736972a7b393b38d7b5cffe571c2009058
tomheatwole <tgh8xn@virginia.edu>
2018-12-15 16:09:34 -0500

    Encapsulate Token.java

--- Token.java
+++ Token.java
@@ -1,12 +1,38 @@
 public class Token {
 
-    public String value;
-    public TokenType type;
-    public int lineNumber;
+    private String value;
+    private TokenType type;
+    private int lineNumber;
 
     public Token(String value, TokenType type, int lineNumber) {
         this.value = value;
         this.type = type;
         this.lineNumber = lineNumber;
+    }
+
+    /* uncomment as needed
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public void setLineNumber(String lineNumber) {
+        this.lineNumber = lineNumber;
+    }
+    */
+
+    public String getValue() {
+        return value;
+    }
+
+    public TokenType getType() {
+        return type;
+    }
+
+    public int getLineNumber() {
+        return lineNumber;
     }
 }
