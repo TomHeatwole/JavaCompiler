515b4d6c91efdef58b7614c6d9b8c9c1bf727dab
tomheatwole <tgh8xn@virginia.edu>
2018-12-30 23:52:04 -0500

    Restructure test_all into additional file

--- test_all.sh
+++ test_all.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-javac Compile.java lexer/*  parser/*.java generator/*
-mv parser/*.class ./
-mv lexer/*.class ./
-mv generator/*.class ./
-java Compile Test.java
+./compile_move_test Test.java
 ./assemble_link_run Test.s
 rm *.class
